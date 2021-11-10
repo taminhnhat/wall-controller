a663c61dd7cbf8b2719480f1ee090935d7f5baf5
taminhnhat <46064428+taminhnhat@users.noreply.github.com>
2021-11-11 01:05:10 +0700

    Update wall-gateway.sh

--- service/wall-gateway.sh
+++ service/wall-gateway.sh
@@ -2,10 +2,6 @@
 
 mkfifo /tmp/emit_gpio
 mkfifo /tmp/gpio_callback
-sudo chmod 777 /tmp/emit_gpio
-sudo chmod 777 /tmp/gpio_callback
-sudo echo '' > /var/log/wall-controller/gpio.log
-sudo echo '' > /var/log/wall-controller/gateway.log
-sudo chmod 777 /var/log/wall-controller/gpio.log
-sudo chmod 777 /var/log/wall-controller/gateway.log
-sudo node /home/ubuntu/wall-controller/index.js > /var/log/wall-controller/gateway.log
+chmod 777 /tmp/emit_gpio
+chmod 777 /tmp/gpio_callback
+sudo node /home/ubuntu/wall-controller/index.js > ~/gateway.log
