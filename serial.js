ae4ef82db2b2962e956ffb46fb1249c80ee997c0
taminhnhat <taminhnhat98@gmail.com>
2022-07-14 10:23:17 +0700

    i

--- serial.js
+++ serial.js
@@ -241,5 +241,4 @@ function rgbHubCheckHealth() {
 
 // setInterval(frontScannerCheckHealth, 5000);
 // setInterval(backScannerCheckHealth, 5000);
-// setInterval(rgbHubCheckHealth, 5000);
-// rgbHubCheckHealth();
+// setInterval(rgbHubCheckHealth, 60000);
